987dd4fc68ae2691badb9dd4d5efffca973d5ddd
Alonso <alonso.moros@alumnos.upm.es>
2024-09-23 17:06:59 +0200

    Updated TectitePillarBlock (Custom)

---
+++ src/main/java/net/alar/dotr/block/custom/TectitePillarBlock.java
@@ -0,0 +1,14 @@
+package net.alar.dotr.block.custom;
+
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+
+public class TectitePillarBlock extends RotatedPillarBlock {
+
+    public TectitePillarBlock(Properties properties) {
+        super(properties);
+    }
+}
