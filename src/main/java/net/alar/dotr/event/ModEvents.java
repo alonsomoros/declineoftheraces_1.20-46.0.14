7edd20c45d32f22b7d9001dda07a9309186baff3
Alonso <alonso.moros@alumnos.upm.es>
2024-09-23 02:27:16 +0200

    VillagersStartUp classes implemented

---
+++ src/main/java/net/alar/dotr/event/ModEvents.java
@@ -0,0 +1,12 @@
+package net.alar.dotr.event;
+
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = "dotr")
+public class ModEvents {
+    public static void addCustomTrades(VillagerTradesEvent event) {
+        // Add custom trades here
+        // Video 10 - 10:07
+    }
+}
---
+++ src/main/java/net/alar/dotr/villager/ModVillagers.java
@@ -0,0 +1,7 @@
+package net.alar.dotr.villager;
+
+public class ModVillagers {
+    public static void registerPOIs() {
+
+    }
+}
