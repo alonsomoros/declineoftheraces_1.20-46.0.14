cf77a3b8b1dcd9d3764a0d37882a615346e5b1b7
Alonso <alonso.moros@alumnos.upm.es>
2024-09-23 02:10:26 +0200

    SilverRoseGarden fixed & implemented

---
+++ src/main/java/net/alar/dotr/block/custom/SilverRoseGarden.java
@@ -0,0 +1,110 @@
+package net.alar.dotr.block.custom;
+
+import net.alar.dotr.block.ModBlocks;
+import net.alar.dotr.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.SweetBerryBushBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class SilverRoseGarden extends BushBlock {
+    private static final float HURT_SPEED_THRESHOLD = 0.003F;
+    public static final int MAX_AGE = 3;
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_3;
+    private static final VoxelShape SAPLING_SHAPE = Block.box(3.0D, 0.0D, 3.0D, 13.0D, 8.0D, 13.0D);
+    private static final VoxelShape MID_GROWTH_SHAPE = Block.box(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D);
+
+    public SilverRoseGarden(BlockBehaviour.Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(AGE, Integer.valueOf(0)));
+    }
+
+    public ItemStack getCloneItemStack(BlockGetter block, BlockPos blockPos, BlockState state) {
+        return new ItemStack(ModBlocks.SILVER_ROSE_GARDEN_BUSH.get());
+    }
+
+    public VoxelShape getShape(BlockState state, BlockGetter block, BlockPos pos, CollisionContext collisionContext) {
+        if (state.getValue(AGE) == 0) {
+            return SAPLING_SHAPE;
+        } else {
+            return state.getValue(AGE) < 3 ? MID_GROWTH_SHAPE : super.getShape(state, block, pos, collisionContext);
+        }
+    }
+
+    public boolean isRandomlyTicking(BlockState state) {
+        return state.getValue(AGE) < 3;
+    }
+
+    public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource source) {
+        int i = state.getValue(AGE);
+        if (i < 3 && level.getRawBrightness(pos.above(), 0) >= 9 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(level, pos, state, source.nextInt(5) == 0)) {
+            BlockState blockstate = state.setValue(AGE, Integer.valueOf(i + 1));
+            level.setBlock(pos, blockstate, 2);
+            level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(blockstate));
+            net.minecraftforge.common.ForgeHooks.onCropsGrowPost(level, pos, state);
+        }
+
+    }
+
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+        if (entity instanceof LivingEntity && entity.getType() != EntityType.FOX && entity.getType() != EntityType.BEE) {
+            entity.makeStuckInBlock(state, new Vec3((double)0.8F, 0.75D, (double)0.8F));
+            if (!level.isClientSide && state.getValue(AGE) > 0 && (entity.xOld != entity.getX() || entity.zOld != entity.getZ())) {
+                double d0 = Math.abs(entity.getX() - entity.xOld);
+                double d1 = Math.abs(entity.getZ() - entity.zOld);
+                if (d0 >= (double)0.003F || d1 >= (double)0.003F) {
+                    entity.hurt(DamageSource.SWEET_BERRY_BUSH, 1.0F);
+                }
+            }
+
+        }
+    }
+
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult blockHitResult) {
+        int i = state.getValue(AGE);
+        boolean flag = i == 3;
+        if (!flag && player.getItemInHand(hand).is(Items.BONE_MEAL)) {
+            return InteractionResult.PASS;
+        } else if (i > 1) {
+            int j = 1 + level.random.nextInt(2);
+            popResource(level, pos, new ItemStack(ModItems.TITANIUM_PETAL.get(), j + (flag ? 1 : 0)));
+            level.playSound((Player)null, pos, SoundEvents.SWEET_BERRY_BUSH_PICK_BERRIES, SoundSource.BLOCKS, 1.0F, 0.8F + level.random.nextFloat() * 0.4F);
+            BlockState blockstate = state.setValue(AGE, Integer.valueOf(1));
+            level.setBlock(pos, blockstate, 2);
+            level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(player, blockstate));
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        } else {
+            return super.use(state, level, pos, player, hand, blockHitResult);
+        }
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> blockStateBuilder) {
+        blockStateBuilder.add(AGE);
+    }
+}
